df6c4e1ac7a89b8c9f8c3e328348917c176c0da6
ElenLT <elenaltm@gmail.com>
2022-05-08 23:23:39 +0200

    adding new tests and some function

--- helpers/some/some.test.js
+++ helpers/some/some.test.js
@@ -4,11 +4,11 @@ import { someFunction } from './some';
 const arrayIsOk = [1, 2, 3];
 const testArrIsEmpty = [];
 const testElementIsNull = [null];
-const IsNotAnArrIsUndefined = undefined;
-const IsNotAnArrIsNumber = 1;
-const IsNotAnArrIsString = 'hello';
+const isNotAnArrIsUndefined = undefined;
+const isNotAnArrIsNumber = 1;
+const isNotAnArrIsString = 'hello';
 
-describe('Give different inputs I expect differen outputs', () => {
+describe('Give different inputs I expect different outputs', () => {
   // true cases
   describe('When I pass an array with items and try finding an element which is included', () => {
     test('Should return true', () => {
@@ -23,8 +23,82 @@ describe('Give different inputs I expect differen outputs', () => {
     test('Should return false', () => {
       // Inputs
       const arr = arrayIsOk;
-      const element = 4;
+      const element = 7;
       const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+  describe('When I pass an empty array', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = testArrIsEmpty;
+      const element = 2;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('When an element is null', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = arrayIsOk;
+      const element = testElementIsNull;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('When the array is undefined', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = isNotAnArrIsUndefined;
+      const element = 2;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+  describe('When the array is not an array but a number', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = isNotAnArrIsNumber;
+      const element = 2;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+  describe('When the array is not an array but a string', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = isNotAnArrIsString;
+      const element = 2;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+  describe('When the element is undefined', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = arrayIsOk;
+      const element = undefined;
+      const result = someFunction(arr, element);
+      console.log(result);
+      expect(result).toBe(false);
+    });
+  });
+
+  describe('When the element is NaN', () => {
+    test('Should return false', () => {
+      // Inputs
+      const arr = arrayIsOk;
+      const element = NaN;
+      const result = someFunction(arr, element);
+      console.log(result);
       expect(result).toBe(false);
     });
   });
