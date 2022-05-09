fbad8da0becb12b8044ef69063b05908b37b3a93
JonPittaluga <jon.pittaluga@gmail.com>
2022-05-09 18:34:35 +0200

    Refined test descriptions in unshift

--- helpers/unshift/unshift.js
+++ helpers/unshift/unshift.js
@@ -14,17 +14,18 @@ export const unshiftMethod = (arr, ...itemsToUnshift) => {
 
   const arrTemp = [];
   arrTemp.length = arr.length + itemsToUnshift.length;
-  console.log(arrTemp.length);
 
+  // add the arr content at the end of the temporary array
   for (let i = arr.length - 1; i >= 0; i--) {
     arrTemp[i + itemsToUnshift.length] = arr[i];
   }
 
+  // add the items at the begining of the arr
   for (let j = itemsToUnshift.length - 1; j >= 0; j--) {
     arrTemp[j] = itemsToUnshift[j];
   }
 
-  console.log('arrTemp after loops', arrTemp);
+  // console.log('arrTemp after loops', arrTemp);
   arr = arrTemp;
   console.log('arr value afer after mutation', arr);
 
--- helpers/unshift/unshift.test.js
+++ helpers/unshift/unshift.test.js
@@ -2,6 +2,7 @@ import { unshiftMethod } from './unshift';
 
 describe('Given different inputs I expect different outputs', () => {
   // invalid input cases
+  // they all respond to the condition !Array.isArray(arr)
   describe('When the arr is not an array', () => {
     test('Shoud return nothing and print a console message', () => {
       const inputNull = null;
@@ -21,61 +22,24 @@ describe('Given different inputs I expect different outputs', () => {
       expect(resultE).toBe();
     });
   });
-  // describe('When the input is not an array and is undefined', () => {
-  //   test('Shoud return nothing and print a console message', () => {
-  //     const input = undefined;
-  //     const result = shiftMethod(input);
-  //     expect(result).toBe();
-  //   });
-  // });
-  // describe('When the input is not an array and is a number', () => {
-  //   test('Shoud return nothing and print a console message', () => {
-  //     const input = 10042014;
-  //     const result = shiftMethod(input);
-  //     expect(result).toBe();
-  //   });
-  // });
-  // describe('When the input is not an array and is a string', () => {
-  //   test('Shoud return nothing and print a console message', () => {
-  //     const input = "I'm a lonely string";
-  //     const result = shiftMethod(input);
-  //     expect(result).toBe();
-  //   });
-  // });
-  // describe('When the input is not an array and is NaN', () => {
-  //   test('Shoud return nothing and print a console message', () => {
-  //     const input = NaN;
-  //     const result = shiftMethod(input);
-  //     expect(result).toBe();
-  //   });
-  // });
-
-  // // valid cases
-  // describe('When the input is an empty array', () => {
-  //   test('Should return undefined', () => {
-  //     const input = [];
-  //     const result = shiftMethod(input);
-  //     expect(result).toBe(undefined);
-  //   });
-  // });
   describe('When the input is one single item', () => {
-    test('Should return the array length and print a console message ', () => {
-      const arr = ['arr0'];
-      const input = 'new item for the array';
+    test('Should return the arr length after being mutated (+1) and print in the console the mutated arr', () => {
+      const arr = ["I'm feeling lonely in the arr"];
+      const input = 'I want to join. But only in the [0] index position';
       const result = unshiftMethod(arr, input);
       expect(result).toBe(2);
     });
   });
   describe('When the arr has three and the input is one single item', () => {
-    test('Should return 4 and print a console message ', () => {
+    test('Should return 4 and print a console message with the mutated arr', () => {
       const arr = ['arr0', 'arr1', 'arr2'];
-      const input = 'get this item';
+      const input = 'New input item to unshift';
       const result = unshiftMethod(arr, input);
       expect(result).toBe(4);
     });
   });
   describe('When the arr has more than 1 element and the input is more than 1 item', () => {
-    test('Should return the array length and print a console message ', () => {
+    test('Should return the array length and print a console message with the mutated arr', () => {
       const arr = [4, 5, 6];
       const inputOne = 1;
       const inputTwo = 2;
@@ -94,7 +58,7 @@ describe('Given different inputs I expect different outputs', () => {
       expect(result).toBe(3);
     });
   });
-  describe('When the arr has N elements and the input is empty', () => {
+  describe('When the arr is empty and the input >1', () => {
     test('Should return the array length and print a console message ', () => {
       const arr = [];
       const inputOne = 1;
