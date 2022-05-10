24a07302a580a6a0dfdd1619ee81b673f49cdfd0
JonPittaluga <jon.pittaluga@gmail.com>
2022-05-10 12:45:38 +0200

    Add tests with exceptions-errors in pop.js and its tests. Includes notes for Elena

--- helpers/pop/pop.js
+++ helpers/pop/pop.js
@@ -4,11 +4,35 @@
 // Describe('Remove and return the last element of an array, mutating it')
 
 export const popMethod = (arr) => {
+  // TODO: Check previous version, before exceptions where implemented
+  // if (!Array.isArray(arr)) {
+  //   console.log(
+  //     `Invalid input: this function does not accept '${arr}' as input. Please add an array`
+  //   );
+  //   return;
+  // }
+
+  // TODO: Do we need to implement try-catch?
   if (!Array.isArray(arr)) {
-    console.log(
-      `Invalid input: this function does not accept '${arr}' as input. Please add an array`
-    );
-    return;
+    // handle all invalid inputs and exceptions together
+
+    const myTypeError = new TypeError();
+
+    if (arr === null || arr === undefined) {
+      myTypeError.messageUX = `TypeError: Cannot read properties of '${arr}' (reading unshiftMethod)`;
+      console.log(myTypeError);
+      throw myTypeError;
+    }
+
+    if (
+      arr instanceof Object === true ||
+      typeof arr === 'number' ||
+      typeof arr === 'string'
+    ) {
+      myTypeError.messageIsNotAFunction = `TypeError: ${arr} is not a function`;
+      console.log(myTypeError);
+      throw myTypeError;
+    }
   }
 
   if (arr.length === 0) {
--- helpers/pop/pop.test.js
+++ helpers/pop/pop.test.js
@@ -8,22 +8,37 @@ import { popMethod } from './pop';
 describe('Given different inputs (an array expected) removes the last element from an array and returns that element (and mutates the array)', () => {
   // invalid input cases: Grouped because they all respond to the condition !Array.isArray(arr)
   describe('When the arr is not an array', () => {
-    test('Shoud return nothing and print a console message', () => {
-      const inputNull = null;
-      const result = popMethod(inputNull);
-      expect(result).toBe();
-      const inputUndefined = undefined;
-      const resultB = popMethod(inputUndefined);
-      expect(resultB).toBe();
-      const inputNaN = NaN;
-      const resultC = popMethod(inputNaN);
-      expect(resultC).toBe();
-      const inputNumber = 1004114;
-      const resultD = popMethod(inputNumber);
-      expect(resultD).toBe();
-      const inputObject = {};
-      const resultE = popMethod(inputObject);
-      expect(resultE).toBe();
+    // TODO: Check this previous version, before the exception tests where implemented
+
+    // test('Shoud return nothing and print a console message', () => {
+    //   const inputNull = null;
+    //   const result = popMethod(inputNull);
+    //   expect(result).toBe();
+    //   const inputUndefined = undefined;
+    //   const resultB = popMethod(inputUndefined);
+    //   expect(resultB).toBe();
+    //   const inputNaN = NaN;
+    //   const resultC = popMethod(inputNaN);
+    //   expect(resultC).toBe();
+    //   const inputNumber = 1004114;
+    //   const resultD = popMethod(inputNumber);
+    //   expect(resultD).toBe();
+    //   const inputObject = {};
+    //   const resultE = popMethod(inputObject);
+    //   expect(resultE).toBe();
+    // });
+
+    describe('When the arr is not an array', () => {
+      test('Shoud capture the error', () => {
+        expect(() => {
+          popMethod(undefined); // Both null and undefined work as inputs;
+        }).toThrow(TypeError);
+      });
+      test('Shoud capture the error', () => {
+        expect(() => {
+          popMethod({}); // Also 'number' or 'string' as inputs return the same error;
+        }).toThrow(TypeError);
+      });
     });
   });
   describe('When the arr has a single item', () => {
